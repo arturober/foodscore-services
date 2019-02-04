f1343a19ff10907f2f1ed4e2e9f7aa203d56a033
Arturo Bernal <arturober@gmail.com>
2019-02-04 16:05:22 +0100

    bug

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -5,7 +5,7 @@ import { Restaurant } from '../entities/restaurant.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentsModule } from '../comments/comments.module';
 import { Comment } from '../entities/comment.entity';
-import { CommonsModule } from 'commons/commons.module';
+import { CommonsModule } from '../commons/commons.module';
 import { UsersModule } from '../users/users.module';
 
 @Module({
